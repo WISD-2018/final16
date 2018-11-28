8b53d1ce78ce4dabc191759a1976b180ea20c293
3A532035 <pp93189tw@gmail.com>
2018-11-28 11:06:30 +0800

    default layout v0.1

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,6 @@
+<div id="myfoot_box3">國立勤益科技大學National Chin-Yi University of Technology<br>
+    地址：<a href="/files/11-1000-24.php" title="交通資訊">41170臺中市太平區坪林里中山路二段 57號 (交通資訊)</a>  | <a href="http://www.ncut.edu.tw/ncut/map/" title="勤益校園地圖">校園地圖</a> <br>
+    No.57, Sec. 2, Zhongshan Rd., Taiping Dist., Taichung 41170, Taiwan (R.O.C.)<br>
+    電話: <a href="http://www.ncut.edu.tw/ncut/phone/">(04)23924505 (分機表)</a>FAX:(04) 23923363 <a href="http://voip.tanet.edu.tw/sq_in.asp" title="網路電話">TANet 98110000 </a><br>
+
+    <a href="http://web2.ncut.edu.tw/ezfiles/0/1000/img/949/878419923.pdf">個人資料保護安全政策</a> | <a href="/files/11-1000-2878.php">隱私權聲明</a> | <a href="http://web2.ncut.edu.tw/files/13-1000-35999.php">當事人行使權利方式及 抱怨申訴</a>| <a href="http://web2.ncut.edu.tw/ezfiles/0/1000/img/949/204756527.pdf">個資法17條應公告事項</a></div>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('test.index');
+});
+
+Route::get('/layout', function () {
+    return view('layout.index');
+});
+
+Route::get('/member', function () {
+    return view('member');
+});
+
+Route::get('bar',function(){
+    return view('test.bar');
+});
+
